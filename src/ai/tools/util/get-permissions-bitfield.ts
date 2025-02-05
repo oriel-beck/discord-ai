33c05d2a63fbfe94fa879c1a964df97b2b5a5c4c
oriel-beck <orielbeck@gmail.com>
2025-02-05 13:30:21 +0200

    add util function to convert bitfield

---
+++ src/ai/tools/util/get-permissions-bitfield.ts
@@ -0,0 +1,34 @@
+import OpenAI from 'openai';
+import { ToolFunction } from '../../types.js';
+import { PermissionsBitField } from 'discord.js';
+
+const getPermissionsBitfield: ToolFunction<{ sourceId: string; bitfield: bigint }> = async ({ sourceId, bitfield }) => {
+  const bits = new PermissionsBitField(`${bitfield}`);
+  return { data: `Source: ${sourceId}, permissions: ${bits.toArray().join(', ')}` };
+};
+
+export const definition: OpenAI.Chat.Completions.ChatCompletionTool = {
+  type: 'function',
+  function: {
+    name: 'convert_permissions_bitfield',
+    description: 'Converts a permission bitfield (such as 11264) to a permission names array.',
+    strict: true,
+    parameters: {
+      type: 'object',
+      additionalProperties: false,
+      required: ['sourceId', 'bitfield'],
+      properties: {
+        sourceId: {
+          type: 'string',
+          description: 'The source Id of what needs the bitfield conversation, can be a role, member, or channel Id',
+        },
+        bitfield: {
+          type: 'string',
+          description: 'The bitfield for the permissions of the sourceId',
+        },
+      },
+    },
+  },
+};
+
+export default getPermissionsBitfield;
